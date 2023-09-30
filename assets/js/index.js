e0de30b8a4377c557039f25e28bc8d0bc7965748
iGeek <jesus.cardenas.d@gmail.com>
2023-09-30 13:13:29 -0600

    corte final clase

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,35 @@
 console.log("Entro index.js");
+console.log(piezas);
+
+const inputBusqueda = document.getElementById("inputBusqueda");
+const resultadoBusqueda = document.getElementById("resultadoBusqueda");
+const imgPieza = document.getElementById("imgPieza");
+
+
+function buscar() {
+    console.log("Entro a funcion buscar");
+    const txtBusqueda = inputBusqueda.value;
+    let resultado = piezas.find((pieza) => {
+        return pieza.nombre.toLocaleLowerCase() == txtBusqueda.toLocaleLowerCase();
+    });
+
+    if (resultado == undefined) {
+        // alert("No se encontro la pieza");
+        imgPieza.src = "assets/images/piezas/logo.png";
+        resultadoBusqueda.innerText = "No se encontro la pieza";
+    } else {
+        // aqui si deberia tener la pieza
+        imgPieza.src = resultado.imagen;
+        // resultadoBusqueda.innerHTML = resultado.movimiento;
+        resultadoBusqueda.innerText = resultado.movimiento;
+    }
+
+    console.log(resultado, "resultado");
+}
+
+inputBusqueda.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        console.log("Entro a evento keypress con enter");
+        buscar();
+    }
+});
